a2265efb235e90a5c6916b698424e9ad629ee1eb
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-01 20:49:15 +0100

    added finishing styles and reached MVP

--- user-card/src/FollowerCard.js
+++ user-card/src/FollowerCard.js
@@ -1,35 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Section = styled.section`
-display:flex;
-flex-wrap: wrap;
-`;
-
 const Div = styled.div`
 margin: 10px;
-width: 500px;
+width: 350px;
 box-shadow: 2px 2px 4px 2px;
 `;
 
 const Image = styled.img`
-width: 500px;
-height: fit-content;
+width: 350px;
 `;
 
 class FollowerCard extends React.Component {
 
     render() {
         return (
-            <Section>
+
             <Div>
-                <Image src={this.props.newFollower.avatar_url}/>
+                <Image src={this.props.newFollower.avatar_url} />
                 <p>{this.props.newFollower.login}</p>
                 <p>{this.props.newFollower.html_url}</p>
                 <p>{this.props.newFollower.location}</p>
-                
+
             </Div>
-            </Section>
+
         );
     }
 }
